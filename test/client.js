61664e1a9ab42aca7ea18d9a87bfe29be4966380
Eivan King <4539809+evan-king@users.noreply.github.com>
2018-03-11 22:36:03 -0300

    list missing dependencies

--- test/client.js
+++ test/client.js
@@ -4,6 +4,7 @@ const
     dest = "portableFP",
     through = require('through2');
 
+// browserify transform that redirects portable-fp lookups to the global variable
 module.exports = function (file) {
     return through(function (buf, enc, next) {
         this.push(buf.toString('utf8').replace(orig, dest));
